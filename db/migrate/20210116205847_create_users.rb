083184fa85c610b23693ee983c2d946b86989950
tomBeach <tbeach2k@gmail.com>
2021-01-16 16:17:15 -0500

    user migrations

---
+++ db/migrate/20210116205847_create_users.rb
@@ -0,0 +1,12 @@
+class CreateNewUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :new_users do |t|
+      t.string :firstname
+      t.string :lastname
+      t.string :username
+      t.string :usertype
+
+      t.timestamps
+    end
+  end
+end
