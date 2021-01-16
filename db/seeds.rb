10054695d5421d31dbc18c83fc8a7dce8c7ea892
tomBeach <tbeach2k@gmail.com>
2021-01-16 15:44:09 -0500

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 # == recipe ratings seed
 Rating.create([
  { rating:"1", comment:"favorite" },
@@ -18,31 +10,37 @@ Rating.create([
 puts "@ratings: #{@ratings.inspect}"
 puts "Rating.column_names: #{Rating.column_names.inspect}"
 
-# # == meat, seafood, dessert, soup_stew, vegetarian
-# Category.destroy_all
-# Category.create([
-#  { category:"meat" },
-#  { category:"seafood" },
-#  { category:"dessert" },
-#  { category:"soup_stew" },
-#  { category:"vegetarian" }
-# ])
-# @categories = Category.all
-# puts "@categories: #{@categories.inspect}"
-# puts "Category.column_names: #{Category.column_names.inspect}"
-#
-# # ==  Thai,  Indian,  French,  Mexican,  Chinese,  Caribbean,  Middle Eastern,  American
-# Nationality.destroy_all
-# Nationality.create([
-#  { nationality:"Thai" },
-#  { nationality:"Indian" },
-#  { nationality:"French" },
-#  { nationality:"Mexican" },
-#  { nationality:"Chinese" },
-#  { nationality:"Caribbean" },
-#  { nationality:"Middle Eastern" },
-#  { nationality:"American" }
-# ])
-# @nationalities = Nationality.all
-# puts "@nationalities: #{@nationalities.inspect}"
-# puts "Nationality.column_names: #{Nationality.column_names.inspect}"
+# == meat, seafood, dessert, soup_stew, vegetarian
+Category.destroy_all
+Category.create([
+ { category:"meat" },
+ { category:"seafood" },
+ { category:"dessert" },
+ { category:"soup/stew" },
+ { category:"vegetarian" },
+ { category:"appetizer" },
+ { category:"breakfast" }
+])
+@categories = Category.all
+puts "@categories: #{@categories.inspect}"
+puts "Category.column_names: #{Category.column_names.inspect}"
+
+# ==  Thai,  Indian,  French,  Mexican,  Chinese,  Caribbean,  Middle Eastern,  American
+Nationality.destroy_all
+Nationality.create([
+ { nationality:"Thai" },
+ { nationality:"Indian" },
+ { nationality:"French" },
+ { nationality:"Mexican" },
+ { nationality:"Chinese" },
+ { nationality:"Caribbean" },
+ { nationality:"Middle Eastern" },
+ { nationality:"American" },
+ { nationality:"Italian" },
+ { nationality:"Asian" },
+ { nationality:"Spanish" },
+ { nationality:"African" }
+])
+@nationalities = Nationality.all
+puts "@nationalities: #{@nationalities.inspect}"
+puts "Nationality.column_names: #{Nationality.column_names.inspect}"
